79dd974141663763f77ffd8a0c422ce45efd93ff
Ashley Daniels <ad@Ashleys-MacBook-Air.local>
2026-08-13 21:32:40 -0500

    Prepare ABA at Home 1.0.8 release

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'ABA at Home',
     slug: 'at-home-app',
-    version: "1.0.7",
+    version: "1.0.8",
     orientation: 'portrait',
     scheme: 'abaathome',
     userInterfaceStyle: 'light',
