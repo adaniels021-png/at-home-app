b0e79d93e54b54e4c3aa9dbc48d0ea5cefabed42
Ashley Daniels <ad@Ashleys-MacBook-Air.local>
2026-08-17 05:53:44 -0500

    Bump app version to 1.0.9

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'ABA at Home',
     slug: 'at-home-app',
-    version: "1.0.8",
+    version: "1.0.9",
     orientation: 'portrait',
     scheme: 'abaathome',
     userInterfaceStyle: 'light',
