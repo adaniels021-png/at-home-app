20756c8393ba8fdbaa901674d1613bb044148cf2
Ashley Daniels <ad@Ashleys-MacBook-Air.local>
2026-08-30 22:40:49 -0500

    chore(release): bump version to 1.1.1

--- app.config.js
+++ app.config.js
@@ -4,7 +4,7 @@ export default {
   expo: {
     name: 'ABA at Home',
     slug: 'at-home-app',
-    version: "1.1.0",
+    version: "1.1.1",
     orientation: 'portrait',
     scheme: 'abaathome',
     userInterfaceStyle: 'light',
